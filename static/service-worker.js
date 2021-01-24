5c1773512d7a713ca3b6cb07a0037887b3afcb71
Andres Navarrete <anavarrete@monkeyplusbc.com>
2021-01-24 16:20:31 -0500

    init

---
+++ static/service-worker.js
@@ -0,0 +1,32 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+/* eslint-disable no-undef */
+
+workbox.core.setCacheNameDetails({ prefix: "pwa2" });
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ www/assets/js/chunk-common.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-common"],{"90ea":function(n,o,w){}}]);
+//# sourceMappingURL=chunk-common.js.map
